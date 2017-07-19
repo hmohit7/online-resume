0ad3137c85530c93f82f2319495da65cd592b083
harsh vidyarthi <hmohit7@users.noreply.github.com>
2017-07-20 01:32:56 +0530

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,92 +1,88 @@
  function contactDetail() {
-     var name = "Harsh Vidyarthi";
-     var role = "Frontend Ninja";
-     var formattedRole = HTMLheaderRole.replace("%data%", role);
-     $("#header").prepend(formattedRole);
-     var formattedName = HTMLheaderName.replace("%data%", name);
-     $("#header").prepend(formattedName);
+     var bio = {
+         name: "Harsh Vidyarthi",
+         role: "Frontend Ninja",
+         contacts: {
+             mobile: "+919880013407",
+             email: "hmohit7@gmail.com",
+             github: "hmohit7",
+             twitter: "@hmohit7",
+             location:"New Delhi, India"
+         },
+         welcomeMessage: "Hi! I'm Harsh a Graphic Designer and Web Developer",
+         skills: ["HTML5", "CSS3", "Javascript", "CanvasJS", "Adobe Creative Suite"],
+         bioPic: "images/f.jpg",
 
-     var mobile = "+919880013407";
-     var contactmobile = HTMLmobile.replace("%data%", mobile);
-     $("#header").append(contactmobile);
 
-     var email = "hmohit7@gmail.com";
-     var contactemail = HTMLemail.replace("%data%", email);
-     $("#header").append(contactemail);
+         display: function() {
+             var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+             var formattedName = HTMLheaderName.replace("%data%", bio.name);
+             var contactgit = HTMLgithub.replace("%data%", bio.github);
+             var formattedpic = HTMLbioPic.replace("%data%", bio.bioPic);
+             var contactwelmsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+             var contactemail = HTMLemail.replace("%data%", bio.contacts.email);
+             var contactmobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+             var contacttwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+             var contactlocation = HTMLlocation.replace("%data%", bio.contacts.location);
+             var allContacts = contactemail + contactmobile +contactgit +contacttwitter+contactlocation;
 
-     var twitter = "twitter/mohitvidyarthi";
-     var contacttwitter = HTMLtwitter.replace("%data%", twitter);
-     $("#header").append(contacttwitter);
+             $("#header").append(formattedName);
+             $("#header").append(formattedRole);
+             $("#header").append(contactmobile);
+             $("#header").append(contactemail);
+             $("#header").append(contacttwitter);
+             $("#header").append(contactgit);
+             $("#header").append(contactlocation);
+             $("#header").append(formattedpic);
+             $("#header").append(contactwelmsg);
+             $("#footerContacts").append(allContacts);
 
-     var github = "github.com/hmohit7";
-     var contactgit = HTMLgithub.replace("%data%", github);
-     $("#header").append(contactgit);
-
-     var conlocation = "Bangalore";
-     var contactlocation = HTMLlocation.replace("%data%", conlocation);
-     $("#header").append(contactlocation);
-     var formattedpic = HTMLbioPic.replace("%data%", "images/f.jpg");
-     $("#header").prepend(formattedpic);
-
-     var welcomemsg = "To work in a dynamic environment which fuels my professional growth and to enhance my skills to the best, to contribute towards company’s growth.";
-     var contactwelmsg = HTMLwelcomeMsg.replace("%data%", welcomemsg);
-     $("#header").append(contactwelmsg);
- }
- contactDetail();
-
- var bio = {
-     "name": "Harsh Vidyarthi",
-     "role": "Frontend webdeveloper",
-     "contact": 9880013407,
-     "message": "welcome to my website",
-     "age": 22,
-     "skills": ["javascript", "teaching", "DBMS", "php", "GYM"]
- };
-
- if (bio.skills.length > 0) {
-
-     $("#header").append(HTMLskillsStart);
-     bio.skills.forEach(function skill(cv, index, array) {
-         var formattedSkill = HTMLskills.replace("%data%", bio.skills[index]);
-         $("#skills").append(formattedSkill);
-     });
+             if (bio.skills.length > 0) {
+                 $("#header").append(HTMLskillsStart);
+                 bio.skills.forEach(function skill(cv, index, array) {
+                     var formattedSkill = HTMLskills.replace("%data%", bio.skills[index]);
+                     $("#skills").append(formattedSkill);
+                 });
+             }
+         }
+     };
+     bio.display();
  }
 
  var work = {
      "jobs": [{
              "employer": "Udacity",
              "title": "Course Developer",
-             "location": "Mountain View, CA",
+             "location": "New Delhi,India",
              "dates": "Feb 2014 - Current",
              "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
          },
          {
              "employer": "LearnBIG",
              "title": "Software Engineer",
-             "location": "Seattle, WA",
+             "location": "Bombay, India",
              "dates": "May 2013 - Jan 2014",
              "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
          },
          {
              "employer": "LEAD Academy Charter High School",
              "title": "Science Teacher",
-             "location": "Nashville, TN",
+             "location": "Bangalore,India",
              "dates": "Jul 2012 - May 2013",
              "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
          },
          {
              "employer": "Stratford High School",
-             "title": "Science Teacher",
-             "location": "Nashville, TN",
+             "title": "Science Teacher India",
+             "location": "Bangalore,India",
              "dates": "Jun 2009 - Jun 2012",
              "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
          }
      ],
      "display": function() {
+         $("#workExperience").append(HTMLworkStart);
 
-         for (var job in work.jobs) {
-             $("#workExperience").append(HTMLworkStart);
-
+         work.jobs.forEach(function(cv, job, array) {
              var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
              var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
              var formattedEmployerTitle = formattedEmployer + formattedTitle;
@@ -103,45 +99,46 @@
 
              var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
              $(".work-entry:last").append(formattedLocation);
-         }
+         });
      }
  };
- work.display();
+
+
+
 
  var projects = {
-     project: [{
-         title: " animal fendt ",
+     projects: [{
+         title: "animal fendt ",
          dates: "20th may 2017",
          description: "Lorem ipsum dolor sit amet, case doctus partiendo vis ad, an oporteat vulputate ius, in sea quis petentium assueverit. In viris exerci propriae qui, duis quaeque et quo. Ne dicta saperet vel, possim aeterno scriptorem cu nec. Quo exerci quaestio dissentiunt eu, eum in dico mollis expetendis. Eam et facilisis urbanitas, postea partiendo vel an.",
          image: [
 
-             "https://www.planwallpaper.com/static/images/offset_WaterHouseMarineImages_62652-2-660x440.jpg",
-             "https://www.planwallpaper.com/static/images/images_1_05GM1zY.jpg"
-
+             "images/k.jpg",
+             "images/port.jpg"
          ]
      }],
      display: function() {
-         for (var pro in projects.project) {
-             $("#projects").append(HTMLprojectStart);
-             var projectTitle = HTMLprojectTitle.replace("%data%", projects.project[pro].title);
+         $("#projects").append(HTMLprojectStart);
+         projects.projects.forEach(function(cv, pro, array) {
+             var projectTitle = HTMLprojectTitle.replace("%data%", projects.projects[pro].title);
              $(".project-entry:last").append(projectTitle);
-             var projectDates = HTMLprojectDates.replace("%data%", projects.project[pro].dates);
+             var projectDates = HTMLprojectDates.replace("%data%", projects.projects[pro].dates);
              $(".project-entry:last").append(projectDates);
 
-             var projectDescription = HTMLprojectDescription.replace("%data%", projects.project[pro].description);
+             var projectDescription = HTMLprojectDescription.replace("%data%", projects.projects[pro].description);
              $(".project-entry:last").append(projectDescription);
 
-             var projectImage = HTMLprojectImage.replace("%data%", projects.project[pro].image[0]);
+             var projectImage = HTMLprojectImage.replace("%data%", projects.projects[pro].image[0]);
              $(".project-entry:last").append(projectImage);
 
-             var projectImage2 = HTMLprojectImage.replace("%data%", projects.project[pro].image[1]);
+             var projectImage2 = HTMLprojectImage.replace("%data%", projects.projects[pro].image[1]);
              $(".project-entry:last").append(projectImage2);
 
 
-         }
+         });
      }
  };
- projects.display();
+
 
 
  var education = {
@@ -150,26 +147,26 @@
              location: "bettiah",
              degree: "sslc",
              dates: "15th may 2014",
-             major: "cse"
+             majors: ["cse"]
          },
          {
              name: "ms public school",
              location: "karnataka",
              degree: "sslc",
              dates: "15th may 2014",
-             major: "cse"
+             majors: ["cse"]
          },
          {
              name: "rnsit public school",
              location: "bangalore",
              degree: "hsslc",
              dates: "15th may 2014",
-             major: "cse"
+             majors: ["cse"]
          }
      ],
 
 
-     onlinecourse: [{
+     onlineCourse: [{
              title: "javascript",
              school: "w3school",
              dates: "14th nov 2010",
@@ -186,10 +183,7 @@
      ],
      display: function() {
          $("#education").append(HTMLschoolStart);
-
-
-
-         for (var edu in education.schools) {
+         education.schools.forEach(function(cv, edu, array) {
              var schoolname = HTMLschoolName.replace("%data%", education.schools[edu].name);
              $(".education-entry:last").append(schoolname);
 
@@ -202,39 +196,41 @@
              var schoollocation = HTMLschoolLocation.replace("%data%", education.schools[edu].location);
              $(".education-entry:last").append(schoollocation);
 
-             var schoolmajor = HTMLschoolMajor.replace("%data%", education.schools[edu].major);
+             var schoolmajor = HTMLschoolMajor.replace("%data%", education.schools[edu].majors);
              $(".education-entry:last").append(schoolmajor);
 
 
-         }
+         });
          $(".educati").append(HTMLonlineClasses);
-         for (var i = 0; i < education.onlinecourse.length; i++) {
-             var onlinetitle = HTMLonlineTitle.replace("%data%", education.onlinecourse[i].title);
+         education.onlineCourse.forEach(function(cv, i, array) {
+             var onlinetitle = HTMLonlineTitle.replace("%data%", education.onlineCourse[i].title);
              $(".educati").append(onlinetitle);
 
-             var onlineschool = HTMLonlineSchool.replace("%data%", education.onlinecourse[i].school);
+             var onlineschool = HTMLonlineSchool.replace("%data%", education.onlineCourse[i].school);
              $(".educati").append(onlineschool);
 
-             var onlinedate = HTMLonlineDates.replace("%data%", education.onlinecourse[i].dates);
+             var onlinedate = HTMLonlineDates.replace("%data%", education.onlineCourse[i].dates);
              $(".educati").append(onlinedate);
 
-             var onlineurl = HTMLonlineURL.replace("%data%", education.onlinecourse[i].url);
+             var onlineurl = HTMLonlineURL.replace("%data%", education.onlineCourse[i].url);
              $(".educati").append(onlineurl);
 
-         }
+         });
 
 
      }
 
-
  };
- education.display();
-$("#mapDiv").append(googleMap);
 
-
+ $("#mapDiv").append(googleMap);
  $(document).click(function(loc) {
      // your code goes here
      var x = loc.pageX;
      var y = loc.pageY;
      logClicks(x, y);
  });
+
+ contactDetail();
+ work.display();
+ projects.display();
+ education.display();
